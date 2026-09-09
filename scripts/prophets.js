f18ee0a32b86e426dc1d93d9dd82ec4c6533d717
tadiwanashedombe <dombeseant@gmail.com>
2026-09-09 22:25:58 +0200

    js and htlmlcomplete

---
+++ scripts/prophets.js
@@ -0,0 +1,44 @@
+const url = "https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json";
+const cards = document.querySelector("#cards");
+
+async function getProphetData() {
+    const response  = await fetch(url);
+    const data = await response.json();
+
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach(prophet => {
+        const card = document.createElement("section");
+        const fullName = document.createElement("h2");
+        const portrait = document.createElement("img");
+        const dob = document.createElement("p");
+        const placeOfBirth = document.createElement("p");
+
+        fullName.textContent =` ${prophet.name} ${prophet.lastname}`;
+        dob.textContent = `Date of Birth: ${prophet.birthdate}`;
+        placeOfBirth.textContent = `Place Of Birth: ${prophet.birthplace}`;
+
+        
+        portrait.setAttribute("src",prophet.imageurl);
+        portrait.setAttribute("alt",`Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute("loading","lazy");
+        portrait.setAttribute("width","180");
+        portrait.setAttribute("height","180");
+
+
+        card.appendChild(fullName);
+        card.appendChild(dob);
+        card.appendChild(placeOfBirth);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+
+};
+
+getProphetData();
+
+
