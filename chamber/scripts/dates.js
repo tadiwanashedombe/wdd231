1b0926c38c6431e0a0439b23ec04db727b168611
tadiwanashedombe <dombeseant@gmail.com>
2026-09-12 14:19:42 +0200

    wide view complete

---
+++ chamber/scripts/dates.js
@@ -0,0 +1,3 @@
+document.querySelector('#currentyear').innerHTML = `&copy;${new Date().getFullYear()} Chitungwiza Chamber of Commerce`;
+
+document.querySelector('#lastModified').innerHTML = `Last Modified: ${document.lastModified}`;
