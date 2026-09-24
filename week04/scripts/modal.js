5c6f7c28213fcdb432bcd60e9c055e2a589f2faf
tadiwanashedombe <dombeseant@gmail.com>
2026-09-24 21:26:27 +0200

    animation and transition

---
+++ week04/scripts/modal.js
@@ -0,0 +1,8 @@
+const modal = document.querySelector("#myModal");
+const closeModal = document.querySelector("#closeModal");
+
+modal.showModal();
+
+closeModal.addEventListener('click', ()=> {
+    modal.close();
+});
