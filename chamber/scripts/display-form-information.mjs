c24cce6d19b348feb91f3f40571c0698d3b911ae
tadiwanashedombe <dombeseant@gmail.com>
2026-09-25 06:36:36 +0200

    js complete

---
+++ chamber/scripts/display-form-information.mjs
@@ -0,0 +1,22 @@
+const info = new URLSearchParams(window.location.search);
+const container = document.querySelector(".thankyou");
+const fmt = (iso) => {
+  const d = new Date(iso);
+  return isNaN(d) ? "—" : d.toLocaleString();
+};
+
+
+export function thankyou() {
+
+    container.innerHTML = `
+        <h2>${info.get("firstName")} ${info.get("lastName")}</h2>
+        <div class="contact-details">
+            <p><strong>Email : </strong> ${info.get("email")}</p>
+            <p><strong>Mobile Number : </strong> ${info.get("mobilePhone")}</p>
+            <p><strong>Business Name : </strong> ${info.get("businessName")}</p>
+            <p><strong>Timestamp :</strong> ${fmt(info.get("timestamp"))}</p>
+        </div>
+    `;
+}
+
+// http://127.0.0.1:5501/wdd231/chamber/thankyou.html?firstName=Tadiwanashe&lastName=Dombe&orgTitle=The+Developers&email=dombes163%40gmail.com&mobilePhone=0985364227&businessName=The+Developers&level=bronze&description=this+is+te+best&timestamp=
--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,7 +1,9 @@
 import { getCurrentWeather } from "./current-weather.mjs";
 import { getForecast } from "./forecast-weather.mjs";
 import { topMembers } from "./members.js";
+import { thankyou } from "./display-form-information.mjs";
 
 getCurrentWeather();
 getForecast();
 topMembers();
+thankyou();
